79ff110c3e603340f5d3186cb78c1dde8da8739a
Devansh Kumar Jha <77242838+dkjha20@users.noreply.github.com>
2021-06-19 23:22:53 +0530

    Update watchdog.rs

--- src/watchdog.rs
+++ src/watchdog.rs
@@ -13,19 +13,16 @@ pub struct Watchdog {
 
 impl Watchdog {
     pub unsafe fn new() -> &'static mut Watchdog {
-        // You can see the starting address in section 23.7 of the manual i.e. 4005_2000.
+        // Returns the reference to the structure's first register
         &mut *(0x40052000 as *mut Watchdog)
     }
 
     pub fn disable(&mut self) {
         unsafe {
             Watchdog *ptr= new();
-            // Disable the watchdog. This has 2 parts, unlocking the watchdog for modification and then disabling the watchdog.
-            // See section 23.3.1 for unlocking the watchdog. Ignore point 3 there.
             core::ptr::write_volatile(&mut self.unlock, 0xC520);
             core::ptr::write_volatile(&mut self.unlock, 0xD928);
             __nop();
-            // To disable the watchdog, see section 23.7.1 and scroll down to the last item in the table the 0th bit to understand how to disable the watchdog. This makes it clear that your operation should only change the 0th bit in the 16-bit value, keeping others same. How would you do that? (Think XOR,AND,OR etc.)
             let mut x = core::ptr::read_volatile(&mut self.stctrlh);
             core::ptr::write_volatile(&mut self.stctrlh, x & 0);
         }
